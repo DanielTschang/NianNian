6d30b005657b0884b3440812e7a55951db87d479
danchang11 <danchang11@gapp.nthu.edu.tw>
2023-01-03 21:16:19 +0800

    add function setScale

--- src/game/C_Sprite.hpp
+++ src/game/C_Sprite.hpp
@@ -7,21 +7,23 @@
 
 class C_Sprite : public Component
 {
-    public:
-    	C_Sprite(Object* owner);
+public:
+    C_Sprite(Object* owner);
 
-        void SetTextureAllocator(ResourceAllocator<sf::Texture>* allocator);
-        // Loads a sprite from file.
-        void Load(int id);
-        void Load(const std::string& filePath);  
+    void SetTextureAllocator(ResourceAllocator<sf::Texture>* allocator);
+    // Loads a sprite from file.
+    void Load(int id);
+    void Load(const std::string& filePath);
 
-        // We override the draw method so we can draw our sprite.
-        void Draw(Window& window) override; 
+    // We override the draw method so we can draw our sprite.
+    void Draw(Window& window) override;
 
-        void LateUpdate(float deltatime) override;
-        void SetTextureRect(int x, int y, int width, int height);
-        void SetTextureRect(const sf::IntRect& rect);
-    private:
+    void LateUpdate(float deltatime) override;
+    void SetTextureRect(int x, int y, int width, int height);
+    void SetTextureRect(const sf::IntRect& rect);
+    void SetScale(float x, float y);
+
+private:
         ResourceAllocator<sf::Texture>* allocator;
         sf::Texture texture;
         sf::Sprite sprite;
