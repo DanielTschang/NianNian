d111d6b7f175772391b75c39263916956fd70249
danchang11 <danchang11@gapp.nthu.edu.tw>
2023-01-03 21:18:13 +0800

    refactor a little bit, detail in description
    add run() function, so all the game logic are filled in this class.
    add destructor that delete the window pointer.

--- src/game/Game.cpp
+++ src/game/Game.cpp
@@ -1,32 +1,48 @@
 #include "Game.hpp"
 
-Game::Game() : window("NianNian Adventure")
-{
-    // This creates a smart pointer to a splash screen scene.
-    std::shared_ptr<SceneSplashScreen> splashScreen = 
-        std::make_shared<SceneSplashScreen>(workingDir, sceneManager, window, textureAllocator);
+void Game::initWindow() {
+    this->window = new Window("Default Game Name");
+}
 
-    // This creates a smart pointer to a game screen scene.
-    std::shared_ptr<SceneGame> gameScene = std::make_shared<SceneGame>(workingDir, textureAllocator);
-    std::shared_ptr<SceneMainMenu> menuScene =
-            std::make_shared<SceneMainMenu>(workingDir, sceneManager, window);
-       
-    unsigned int splashScreenID = sceneManager.Add(splashScreen);
-    unsigned int gameSceneID = sceneManager.Add(gameScene);
-    unsigned int menuSceneID= sceneManager.Add(menuScene);
-    
-    // Now that we have our game scene id, we can set the splash screen to transition to the game scene.
-    splashScreen->setSwitchToScene(gameSceneID);
-    
-    // We want the game to start at the splash screen
-    sceneManager.switchTo(splashScreenID);
-    
-    deltaTime = clock.restart().asSeconds();
+void Game::initWindow(std::string WindowName) {
+    this->window = new Window(WindowName);
+}
+
+Game::Game()
+{
+    this->initWindow("NianNianAdventure");
+
+
+
+    // This creates a smart pointer to a splash screen scene.
+//    std::shared_ptr<SceneSplashScreen> splashScreen =
+//        std::make_shared<SceneSplashScreen>(workingDir, sceneManager, window, textureAllocator);
+//
+//    // This creates a smart pointer to a game screen scene.
+//    std::shared_ptr<SceneGame> gameScene = std::make_shared<SceneGame>(workingDir, textureAllocator);
+//    std::shared_ptr<SceneMainMenu> menuScene =
+//            std::make_shared<SceneMainMenu>(workingDir, sceneManager, window);
+//
+//    unsigned int splashScreenID = sceneManager.Add(splashScreen);
+//    unsigned int gameSceneID = sceneManager.Add(gameScene);
+//    unsigned int menuSceneID= sceneManager.Add(menuScene);
+//
+//    // Now that we have our game scene id, we can set the splash screen to transition to the game scene.
+//    splashScreen->setSwitchToScene(gameSceneID);
+//
+//    // We want the game to start at the splash screen
+//    sceneManager.switchTo(splashScreenID);
+//
+//    deltaTime = clock.restart().asSeconds();
+}
+
+Game::~Game() {
+    delete this->window;
 }
 
 void Game::Update()
 {
-    window.Update();
+    window->Update();
     sceneManager.Update(deltaTime);
 }
 
@@ -37,11 +53,11 @@ void Game::LateUpdate()
 
 void Game::Draw()
 {
-    window.BeginDraw();
+    window->BeginDraw(); //sf::window.clear()
     
-    sceneManager.Draw(window);
+    sceneManager.Draw(*window); //scene draw
 
-    window.EndDraw();
+    window->EndDraw(); //sf::window.display
 }
 
 void Game::getInput()
@@ -49,13 +65,33 @@ void Game::getInput()
     sceneManager.processInput();
 }
 
-
+/*
+ * updates the deltatime variable with the time it takes to render and update per frame
+ */
 void Game::CalculateDeltaTime()
 {
-    deltaTime = clock.restart().asSeconds();
+    this->deltaTime = this->deltaTimeClock.restart().asSeconds();
 }
 
 bool Game::IsRunning() const
 {
-    return window.IsOpen();
+    return window->IsOpen();
 }
+
+void Game::run() {
+    while (this->IsRunning())
+    {
+        this->getInput();
+        this->Update();
+        this->LateUpdate();
+        this->Draw();
+        this->CalculateDeltaTime();
+    }
+}
+
+void Game::updateEvents() {
+
+}
+
+
+
--- src/game/Game.hpp
+++ src/game/Game.hpp
@@ -1,10 +1,12 @@
-#ifndef Game_hpp
-#define Game_hpp
+#pragma once
 
 
 #include <iostream>
 #include <unordered_map>
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Network.hpp>
 #include "WorkingDirectory.hpp"
 #include "Window.hpp"
 #include "Input.hpp"
@@ -17,27 +19,35 @@
 
 class Game
 {
+private:
+    void initWindow();
+    void initWindow(std::string WindowName);
+
 public:
+    //Constructor / Destructor
     Game();
+    virtual ~Game();
+
+    //functions
     void getInput();
     void Update();
     void LateUpdate();
     void Draw(); // render
-    void CalculateDeltaTime(); 
+    void CalculateDeltaTime();
+    void run();
+    void updateEvents();
 
     bool IsRunning() const;
     
 private:
-    Window window;
+    Window* window;
     WorkingDirectory workingDir;
 
-    sf::Clock clock;
+    sf::Clock deltaTimeClock;
     float deltaTime;
 
     SceneStateMachine sceneManager;
     ResourceAllocator<sf::Texture> textureAllocator;
-    
+
 };
 
-
-#endif
