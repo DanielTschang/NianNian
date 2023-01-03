4a8c9cb8405cdca1ca8177591c1bf2b0e6bd3640
danchang11 <danchang11@gapp.nthu.edu.tw>
2023-01-03 21:19:53 +0800

    Main.cpp now don't need to go through while loop

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,15 +4,6 @@
 int main()
 {
     Game game;
-    
-    while (game.IsRunning())
-    {
-        game.getInput();
-        game.Update();
-        game.LateUpdate();
-        game.Draw();
-        game.CalculateDeltaTime();
-    }
-    
-    return 0;
+    game.run();
+
 }
